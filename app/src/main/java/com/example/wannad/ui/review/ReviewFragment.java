de6f430a0660d99250bcced15006467e9e914e23
young315 <young315@knu.ac.kr>
2020-11-06 17:21:41 +0900

    add bottom navigation bar

---
+++ app/src/main/java/com/example/wannad/ui/review/ReviewFragment.java
@@ -0,0 +1,35 @@
+package com.example.wannad.ui.review;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.wannad.R;
+
+public class ReviewFragment extends Fragment {
+
+    private ReviewViewModel reviewViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        reviewViewModel =
+                ViewModelProviders.of(this).get(ReviewViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_review);
+        reviewViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
