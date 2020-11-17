838844010898d5e781b532758724b42eb73c6c33
pers <lsj622@naver.com>
2020-11-17 21:46:54 +0900

    review page

--- app/src/main/java/com/example/wannad/ui/review/ReviewFragment.java
+++ app/src/main/java/com/example/wannad/ui/review/ReviewFragment.java
@@ -1,35 +1,58 @@
 package com.example.wannad.ui.review;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RatingBar;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.wannad.BottomNavigation;
+import com.example.wannad.MainActivity;
 import com.example.wannad.R;
 
 public class ReviewFragment extends Fragment {
-
+    Spinner spinner;
     private ReviewViewModel reviewViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         reviewViewModel =
                 ViewModelProviders.of(this).get(ReviewViewModel.class);
         View root = inflater.inflate(R.layout.fragment_review, container, false);
-        final TextView textView = root.findViewById(R.id.text_review);
-        reviewViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        final RatingBar ratingBar = (RatingBar) root.findViewById(R.id.ratingbar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener(){
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(getActivity(),
+                        "New Rating: " + rating, Toast.LENGTH_SHORT).show();
             }
         });
+        spinner = root.findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        adapter.add("test1");
+        adapter.add("test2");
         return root;
+
+
     }
+
+
 }
