5a18b6a5457da8afd5263ab480e9f22f100f89a6
HyeWon <qorgp13@naver.com>
2020-12-14 17:15:55 +0900

    db,,

--- app/src/main/java/com/example/wannad/BottomNavigation.java
+++ app/src/main/java/com/example/wannad/BottomNavigation.java
@@ -1,20 +1,7 @@
 package com.example.wannad;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -24,6 +11,10 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class BottomNavigation extends AppCompatActivity {
     DatabaseReference mDatabase;
@@ -51,8 +42,8 @@ public class BottomNavigation extends AppCompatActivity {
         strNickname = intent.getStringExtra("name");
         strProfile = intent.getStringExtra("profile");
         mDatabase.setValue(strId);
-        mDatabase.child("strId").child("user").child("name").setValue(strNickname);
-        mDatabase.child("strId").child("user").child("profile").setValue(strProfile);
+        mDatabase.child(strId).child("user").child("name").push().setValue(strNickname);
+        mDatabase.child(strId).child("user").child("profile").push().setValue(strProfile);
 
 
     }
